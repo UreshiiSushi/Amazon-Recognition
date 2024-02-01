c4c20a4495b2955a446df0a8517712e24d023b30
UreshiiSushi <merieru@gmail.com>
2024-02-01 22:43:42 +0100

    exercise comprehend done

--- exercise-comprehend/main.py
+++ exercise-comprehend/main.py
@@ -6,14 +6,14 @@ with open("movies.csv", 'r') as fd:
     reader = csv.DictReader(fd, fieldnames=["ResponseId", "Notes"], dialect='excel')
     all_notes = [ row["Notes"] for row in reader ]
 
-client = boto3.client('comprehend')
+client = boto3.client('comprehend', region_name='us-east-1')
 
 #####
 # Complete the call to batch_detect_sentiment
 #####
 response = client.batch_detect_sentiment(
-#     ???
-#     ???
+    TextList = all_notes,
+    LanguageCode = "en",
 )
 
 for result in response["ResultList"]:
